1d9b7f4b216a7a0dedeffce6311df6726b78abb0
Md Irfan Chowdhury <irfanchowdhury80@gmail.com>
2026-08-27 16:16:25 +0600

    added vendor autoload

---
+++ xenarchs-php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'irfan/simple_routing_system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '084d25c24d32e2a27ff8092b8161318bfdb05134',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'irfan/simple_routing_system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '084d25c24d32e2a27ff8092b8161318bfdb05134',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
